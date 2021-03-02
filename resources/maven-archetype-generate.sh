09ceb43d232b5ed69ac65ec471c0bbb7a8cf9e2a
Rob Ratcliffe <rlr@tcliffe.dev>
2021-03-02 10:06:59 -0500

    Add our archetype test

--- resources/maven-archetype-generate.sh
+++ resources/maven-archetype-generate.sh
@@ -3,14 +3,17 @@
 set -o nounset
 set -o errexit
 
-# Change this to be the actual archetype
-
 mvn archetype:generate \
--DarchetypeGroupId=org.apache.maven.archetypes \
--DarchetypeArtifactId=maven-archetype-plugin \
--DarchetypeVersion=1.4 \
+-DarchetypeGroupId=com.ms3-inc.tavros \
+-DarchetypeArtifactId=camel-openapi-archetype \
+-DarchetypeVersion=0.2.6 \
+-DspecificationUri=openapi.yaml
+-DpackageInPathFormat \
+-Dpackage=com.ms3_inc.camel \
 -DgroupId=${GROUP_ID} \
 -DartifactId=${ARTIFACT_ID} \
 -Dversion=${VERSION} \
 -DinteractiveMode=false \
 -DoutputDirectory=..
+
+# add if statement for package in path format based on group id
