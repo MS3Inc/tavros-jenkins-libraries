32bbfca699d59e2f488d7788de07e4cd9ae1c179
Rob Ratcliffe <rlr@tcliffe.dev>
2021-03-02 11:43:48 -0500

    Add underscore check

--- resources/maven-archetype-generate.sh
+++ resources/maven-archetype-generate.sh
@@ -3,17 +3,32 @@
 set -o nounset
 set -o errexit
 
-ls && mvn archetype:generate \
--DarchetypeGroupId=com.ms3-inc.tavros \
--DarchetypeArtifactId=camel-openapi-archetype \
--DarchetypeVersion=0.2.6 \
--DspecificationUri=openapi.yaml \
--DpackageInPathFormat \
--Dpackage=${GROUP_ID} \
--DgroupId=${GROUP_ID} \
--DartifactId=${ARTIFACT_ID} \
--Dversion=${VERSION} \
--DinteractiveMode=false \
--DoutputDirectory=..
+group=${GROUP_ID}
+
+if [[ group == *"-"* ]]; then
+  echo group && mvn archetype:generate \
+  -DarchetypeGroupId=com.ms3-inc.tavros \
+  -DarchetypeArtifactId=camel-openapi-archetype \
+  -DarchetypeVersion=0.2.6 \
+  -DpackageInPathFormat \
+  -Dpackage=${group/-/_} \
+  -DgroupId=com.ms3-inc.camel \
+  -DspecificationUri=openapi.yaml \
+  -DartifactId=${ARTIFACT_ID} \
+  -Dversion=${VERSION} \
+  -DinteractiveMode=false \
+  -DoutputDirectory=..
+else
+  mvn archetype:generate \
+  -DarchetypeGroupId=com.ms3-inc.tavros \
+  -DarchetypeArtifactId=camel-openapi-archetype \
+  -DarchetypeVersion=0.2.6 \
+  -DgroupId=com.ms3-inc.camel \
+  -DspecificationUri=openapi.yaml \
+  -DartifactId=${ARTIFACT_ID} \
+  -Dversion=${VERSION} \
+  -DinteractiveMode=false \
+  -DoutputDirectory=..
+fi
 
 # add if statement for package in path format based on group id
