ff1f8623c9590f0cf11105abd97b9ee4b9f8bd2e
Rob Ratcliffe <rlr@tcliffe.dev>
2021-03-02 13:27:32 -0500

    Simplify if/else

--- resources/maven-archetype-generate.sh
+++ resources/maven-archetype-generate.sh
@@ -3,32 +3,15 @@
 set -o nounset
 set -o errexit
 
-group=${GROUP_ID}
-
-if [[ $group == *"-"* ]]; then
-  mvn archetype:generate \
-  -DarchetypeGroupId=com.ms3-inc.tavros \
-  -DarchetypeArtifactId=camel-openapi-archetype \
-  -DarchetypeVersion=0.2.6 \
-  -DpackageInPathFormat \
-  -Dpackage=${group/-/_} \
-  -DgroupId=$group \
-  -DspecificationUri=openapi.yaml \
-  -DartifactId=${ARTIFACT_ID} \
-  -Dversion=${VERSION} \
-  -DinteractiveMode=false \
-  -DoutputDirectory=..
-else
-  mvn archetype:generate \
-  -DarchetypeGroupId=com.ms3-inc.tavros \
-  -DarchetypeArtifactId=camel-openapi-archetype \
-  -DarchetypeVersion=0.2.6 \
-  -DgroupId=$group \
-  -DspecificationUri=openapi.yaml \
-  -DartifactId=${ARTIFACT_ID} \
-  -Dversion=${VERSION} \
-  -DinteractiveMode=false \
-  -DoutputDirectory=..
-fi
-
-# add if statement for package in path format based on group id
+mvn archetype:generate \
+-DarchetypeGroupId=com.ms3-inc.tavros \
+-DarchetypeArtifactId=camel-openapi-archetype \
+-DarchetypeVersion=0.2.6 \
+$(if [[ ${GROUP_ID} == *"-"* ]]; then echo -DpackageInPathFormat; fi) \
+$(if [[ ${GROUP_ID} == *"-"* ]]; then echo -Dpackage=${GROUP_ID/-/_}; fi) \
+-DgroupId=${GROUP_ID} \
+-DspecificationUri=openapi.yaml \
+-DartifactId=${ARTIFACT_ID} \
+-Dversion=${VERSION} \
+-DinteractiveMode=false \
+-DoutputDirectory=..
