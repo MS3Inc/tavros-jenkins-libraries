e8c5930d94b7c95151c7d049623dc67123708e4b
Rob Ratcliffe <rlr@tcliffe.dev>
2021-03-02 12:21:53 -0500

    Fix bash errors

--- resources/maven-archetype-generate.sh
+++ resources/maven-archetype-generate.sh
@@ -5,14 +5,14 @@ set -o errexit
 
 group=${GROUP_ID}
 
-if [[ group == *"-"* ]]; then
-  echo group && mvn archetype:generate \
+if [[ $group == *"-"* ]]; then
+  mvn archetype:generate \
   -DarchetypeGroupId=com.ms3-inc.tavros \
   -DarchetypeArtifactId=camel-openapi-archetype \
   -DarchetypeVersion=0.2.6 \
   -DpackageInPathFormat \
   -Dpackage=${group/-/_} \
-  -DgroupId=com.ms3-inc.camel \
+  -DgroupId=$group \
   -DspecificationUri=openapi.yaml \
   -DartifactId=${ARTIFACT_ID} \
   -Dversion=${VERSION} \
@@ -23,7 +23,7 @@ else
   -DarchetypeGroupId=com.ms3-inc.tavros \
   -DarchetypeArtifactId=camel-openapi-archetype \
   -DarchetypeVersion=0.2.6 \
-  -DgroupId=com.ms3-inc.camel \
+  -DgroupId=$group \
   -DspecificationUri=openapi.yaml \
   -DartifactId=${ARTIFACT_ID} \
   -Dversion=${VERSION} \
