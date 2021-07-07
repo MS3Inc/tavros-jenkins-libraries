3ff0ad2961b81572a835b22ddf3332c0a40f759c
rmccright-ms3 <rmccright@ms3-inc.com>
2021-07-07 16:14:59 -0400

    Reformat curl call

--- resources/gitea-create-org.sh
+++ resources/gitea-create-org.sh
@@ -4,15 +4,15 @@ set -o nounset
 set -o errexit
 
 curl -sSL -X POST "https://${GIT_HOST}/api/v1/orgs" -i --fail \
-  -u $GIT_CREDS_USR:$GIT_CREDS_PSW \
-  -H "Accept: application/json" \
-  -H "Content-Type: application/json" \
+  -u GIT_CREDS_USR:$GIT_CREDS_PSW \
+  -H  "accept: application/json" \
+  -H  "Content-Type: application/json" \
   -d "{ \
-    \"full_name\": \"${ORG_NAME}\", \
     \"description\": \"${ORG_DESC}\", \
-    \"location\":  \"${ORG_LOC}\", \
+    \"full_name\": \"${ORG_NAME}\", \
+    \"location\": \"${ORG_LOC}\", \
     \"repo_admin_change_team_access\": ${REPO_ADMIN_ACCESS_CONTROL}, \
     \"username\": \"${ORG_USER}\", \
     \"visibility\": \"${ORG_VIS}\", \
-    \"website\": \"${ORG_WEBSITE}\", \
+    \"website\": \"${ORG_WEBSITE}\" \
   }"
