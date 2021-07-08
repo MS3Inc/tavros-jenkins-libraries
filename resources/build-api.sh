98c234010414909e601dbdd3bf5af6c54ace3b89
rmccright-ms3 <rmccright@ms3-inc.com>
2021-07-08 16:32:33 -0400

    update for testing spotify build

--- resources/build-api.sh
+++ resources/build-api.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
 
-docker login ${REG_HOST} -u ${REG_CREDS_USR} -p ${REG_CREDS_PSW}
-docker build -t ${REG_HOST}/rm/test-camel:latest .
-docker push ${REG_HOST}/rm/test-camel:latest
+mvn deploy
