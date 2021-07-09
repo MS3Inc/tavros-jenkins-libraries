577f0b4c327c2334b4e4b59fa9ed7637384a0fa5
rmccright-ms3 <rmccright@ms3-inc.com>
2021-07-09 10:15:02 -0400

    sanity check

--- resources/build-api.sh
+++ resources/build-api.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
-echo This is a test
-echo $REG_HOST
-echo $REG_CREDS_USR
-mvn deploy -Dhost=$REG_HOST -Duser=$REG_CREDS_USR -Dpass=$REG_CREDS_PSW
+
+mvn deploy -Dhost=${REG_HOST} -Duser=${REG_CREDS_USR} -Dpass=${REG_CREDS_PSW}
