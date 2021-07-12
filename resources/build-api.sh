90f76d3141f64e3d30dd89fc9ba5e8f5d6a63122
rmccright-ms3 <rmccright@ms3-inc.com>
2021-07-12 09:44:12 -0400

    Manual export test

--- resources/build-api.sh
+++ resources/build-api.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
+export REG_HOST='test'
 mvn deploy -Dhost=${REG_HOST} -Duser=${REG_CREDS_USR} -Dpass=${REG_CREDS_PSW}
+
+
+# docker login ${REG_HOST} -u ${REG_CREDS_USR} -p ${REG_CREDS_PSW}
+# docker build -t ${REG_HOST}/rm/test-camel:latest .
+# docker push ${REG_HOST}/rm/test-camel:latest
