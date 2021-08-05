844ca9ec74cd2c58bb91966e0e43cd303c721a56
Rob Ratcliffe <rlr@tcliffe.dev>
2021-08-05 10:11:07 -0400

    Try to fix commit message

--- resources/update-helm-release.sh
+++ resources/update-helm-release.sh
@@ -18,6 +18,6 @@ echo ${GIT_URL} \
   && if [[ $GIT_STATUS == *"nothing to commit"* ]]; then
       echo "No changes to helm release version, no need to commit.";
   else
-      git commit -m ${COMMIT_MSG} \
+      git commit -m "${COMMIT_MSG}" \
       && git push -u origin main
   fi
