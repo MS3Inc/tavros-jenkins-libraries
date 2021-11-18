15c59688dec6c6bf8d3563c31f5b364d682e75f6
Rob Ratcliffe <rlr@tcliffe.dev>
2021-11-18 16:15:29 -0500

    Remove maven settings for now

--- vars/camelwebservice.groovy
+++ vars/camelwebservice.groovy
@@ -64,7 +64,6 @@ def call(Map args = [:]) {
                     PROJECT_VERSION = sh(returnStdout: true, script: "mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout").trim()
                     COMMIT_MSG = "Update version"
                     ENV = "${env.DEFAULT_ENV == null ? 'dev' : env.DEFAULT_ENV}"
-                    MVN_SETTINGS = credentials("mvn-settings")
                 }
                 steps {
                     container('git') {
