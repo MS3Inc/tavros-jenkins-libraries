ece45f1daa5430ee50f074d9ceaf8f77c4be419c
Rob Ratcliffe <rlr@tcliffe.dev>
2022-08-18 20:39:54 -0400

    fix typo

--- resources/update-helm-release.sh
+++ resources/update-helm-release.sh
@@ -12,7 +12,7 @@ echo ${GIT_URL} \
   && cd ../.. \
   && git remote remove origin \
   && git remote add origin "https://${GIT_CREDS_USR}:${GIT_CREDS_PSW}@${GIT_HOST}/tavros/platform.git" \
-  && echo "Adding git configuration..."
+  && echo "Adding git configuration..." \
   && git config --global user.email "${BUILD_USER_EMAIL}" \
   && git config --global user.name "${BUILD_USER}" \
   && git config --global --add safe.directory tavros-platform \
