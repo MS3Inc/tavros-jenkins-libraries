a81f2cd60b2a0a8177a5f1d81cbac1325de26b9d
Rob Ratcliffe <rlr@tcliffe.dev>
2023-08-06 16:14:37 -0400

    update for k8s maven plugin

--- resources/maven-deploy.sh
+++ resources/maven-deploy.sh
@@ -2,7 +2,7 @@
 
 FILE=.settings.xml
 if [ -f "$FILE" ]; then
-    printf "\nUsing settings.xml"; mvn -s .settings.xml clean deploy;
+    printf "\nUsing settings.xml"; mvn -s .settings.xml clean package k8s:build k8s:push;
 else
-    printf "\nNot using provided settings.xml"; mvn clean deploy;
+    printf "\nNot using provided settings.xml"; mvn clean clean package k8s:build k8s:push;
 fi
