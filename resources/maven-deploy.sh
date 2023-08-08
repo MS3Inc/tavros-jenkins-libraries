59b8338cad54867b44cfe4c50bb20867f2b471fe
Rob Ratcliffe <rlr@tcliffe.dev>
2023-08-07 21:07:15 -0400

    delete maven deploy

--- resources/maven-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-FILE=.settings.xml
-if [ -f "$FILE" ]; then
-    printf "\nUsing settings.xml"; mvn -s .settings.xml clean package k8s:build k8s:push;
-else
-    printf "\nNot using provided settings.xml"; mvn clean clean package k8s:build k8s:push;
-fi
