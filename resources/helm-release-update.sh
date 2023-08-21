6c128863ac5b07af94ec5d317764d385672e37a4
Rob Ratcliffe <rlr@tcliffe.dev>
2023-08-20 22:19:37 -0400

    refactor to one sed command

--- resources/helm-release-update.sh
+++ resources/helm-release-update.sh
@@ -3,10 +3,11 @@ set -o nounset
 set -o errexit
 
 echo "Making change to ${RELEASE_PATH}" \
-&& sed -i "s/last-commit:.*/last-commit: \"${GIT_COMMIT}\"/g" ${RELEASE_PATH} \
-&& sed -i "s/tag:.*/tag: '${VERSION}'/g" ${RELEASE_PATH} \
-&& sed -i "s/<name>/${NAME}/g" ${RELEASE_PATH} \
-&& sed -i "s/<namespace>/${NAMESPACE}/g" ${RELEASE_PATH} \
-&& sed -i "s/<fqdn>/${TAVROS_HOST}/g" ${RELEASE_PATH} \
+&& sed -i "s/last-commit:.*/last-commit: \"${GIT_COMMIT}\"/g;
+           s/tag:.*/tag: '${VERSION}'/g;
+           s/<name>/${NAME}/g;
+           s/<namespace>/${NAMESPACE}/g;
+           s/<fqdn>/${TAVROS_HOST}/g;
+           " ${RELEASE_PATH} \
 && echo "Output of release.yaml:" \
 && cat ${RELEASE_PATH}
